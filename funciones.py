47442b4a8561e61297f8b1a1a88f9893b1c64789
unknown <jose.meneses@manquemavida.cl>
2023-06-14 10:02:31 -0500

    le puse los valores para identificar si es primo o no

--- funciones.py
+++ funciones.py
@@ -1,5 +1,8 @@
-def numerosprimos():
+def numerosprimos(a):
     a = input("ingresa un numero, ")
-    if a / a == int(5): 
+    if a == 1 or a == 0:
+        return False
+    elif a == 2:
+        return True
+    elif a > 2:
         pass
-    
